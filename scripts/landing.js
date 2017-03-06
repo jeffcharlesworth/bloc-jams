0f37f357e2266e394903a5c0a962f1e12cbd03d3
Jeffrey C. Charlesworth <jcharlesworth02@gmail.com>
2017-03-06 16:02:55 -0700

    Checkpoint 9 dom scripting events complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,34 @@
- var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
-  // takes a single argument: the index of the points class node element does it want function (i) to choose where to start?
-     var revealPoint = function () {
-        for(var i =0; i < points.length; i++) {
-         points[i].style.opacity = 1;
-         points[i].style.transform = "scaleX(1) translateY(0)";
-         points[i].style.msTransform = "scaleX(1) translateY(0)";
-         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        } 
-     };
-     revealPoint();
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+
+
+     var revealPoint = function (index) {   
+         points[index].style.opacity = 1;
+         points[index].style.transform = "scaleX(1) translateY(0)";
+         points[index].style.msTransform = "scaleX(1) translateY(0)";
+         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+         
+     }
+     
+     for(var i =0; i < points.length; i++) {
+          revealPoint(i);
+     }
+
  };
+
+window.onload = function() {
+    if(window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
