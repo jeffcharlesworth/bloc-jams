f92db5503a8437473076ccee4685dddd6de1be0c
Jeffrey C. Charlesworth <jcharlesworth02@gmail.com>
2017-03-13 14:59:38 -0700

    Checkpoint 17 collection and album views Complete

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function () {
+    var template =
     '<div class="collection-album-container column fourth">'
 +   '   <img src="assets/images/album_covers/01.png"/>'
 +   '   <div class="collection-album-info caption">'
@@ -11,14 +12,16 @@ var collectionItemTemplate =
 +   '           <br/>'
 +   '       </p>'
 +   '   </div>'
-+   '</div>'
-;
-window.onload = function() {
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    collectionContainer.innerHTML = '';
++   '</div>';
+  return $(template);
+};
+$(window).load(function() {
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
     
     for(var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbNail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbNail);
     }
     
-}
+});
